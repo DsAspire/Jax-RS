56a0cd036ec63ec9c70ae542db8af7c39715a87b
Damageddream <marcan.grab@gmail.com>
2024-10-08 13:13:21 +0200

    first commit

---
+++ src/main/java/org/example/jaxrs/NotificationsResource.java
@@ -0,0 +1,31 @@
+package org.example.jaxrs;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/notifications")
+public class NotificationsResource {
+    @GET
+    @Path("/ping")
+    public Response ping() {
+        return Response.ok().entity("Service online").build();
+    }
+
+    @GET
+    @Path("/get/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getNotification(@PathParam("id") int id) {
+        return Response.ok()
+                .entity(new Notification(id, "john", "test notification"))
+                .build();
+    }
+
+    @POST
+    @Path("/post/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postNotification(Notification notification) {
+        return Response.status(201).entity(notification).build();
+    }
+}
